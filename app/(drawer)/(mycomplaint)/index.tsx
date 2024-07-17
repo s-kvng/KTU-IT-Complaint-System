2208cfa0f38ea5b79ec137a645dae3788a7e272b
s-kvng <kvngnathan8420@gmail.com>
2024-07-17 23:56:15 +0000

    screen: add my complaint screen

---
+++ app/(drawer)/(mycomplaint)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Index = () => {
+    return (
+        <View>
+            <Text>My compliant</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Index;
